6965f0633272f4137458866470244ad6e8081274
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-11-03 08:59:06 -0300

    57 - Date pipe

--- code-snapshots/02-essentials/21-added-task-template-and-styles/src/app/tasks/task/task.component.ts
+++ code-snapshots/02-essentials/21-added-task-template-and-styles/src/app/tasks/task/task.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 import { Task } from './task.model';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
