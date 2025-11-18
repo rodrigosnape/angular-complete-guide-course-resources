7f547cf25f13632a9a2d9bc456f41158888231b5
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-11-18 11:44:19 -0300

    62 - Salvando no localStorage

--- code-snapshots/02-essentials/21-added-task-template-and-styles/src/app/tasks/tasks.service.ts
+++ code-snapshots/02-essentials/21-added-task-template-and-styles/src/app/tasks/tasks.service.ts
@@ -29,6 +29,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -45,5 +52,10 @@ export class TasksService {
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
